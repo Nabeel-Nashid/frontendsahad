c69d31e2b4aef697ec5e9269ff1bdf8c9b1dacd5
nabeelnashid <53267839+Nabeel-Nashid@users.noreply.github.com>
2023-08-14 16:25:56 +0530

    testttt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import First from "./pages/first";
 import Second from "./pages/second";
 import { Container } from "reactstrap";
+import Third from "./pages/third";
+import Fourth from "./pages/fourth";
 function App() {
   return (
     <div className="App">
       <Container>
         <First />
         <Second />
+        <Third />
+        <Fourth />
       </Container>
     </div>
   );
---
+++ src/pages/third.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Third() {
+  return (
+    <div>
+      3
+    </div>
+  )
+}
+
+export default Third
